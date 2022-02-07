00a5b0fac261069e087688c345bb0149894b5d77
kumudinidesale <kdesale1211@gmail.com>
2022-02-07 21:19:00 +0530

    added: get no between 1to 6

--- src/com/javaproject/SnakeLadder.java
+++ src/com/javaproject/SnakeLadder.java
@@ -1,9 +1,15 @@
 package com.javaproject;
 
+import java.util.Random;
+
 public class SnakeLadder {
 
 	public static void main(String[] args) {
 		int position = 0;
+Random ran = new Random();
+		
+		int dice = ran.nextInt(6)+1;
+		System.out.println("Dice Outcome : "+dice);
 
 	}
 
